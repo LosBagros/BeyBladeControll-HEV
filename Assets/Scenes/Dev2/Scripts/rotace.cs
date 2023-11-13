f8d6b93db9446c2fa5acbe42eae13962cc49ca66
FilipHlavka <113585003+FilipHlavka@users.noreply.github.com>
2023-11-13 09:27:25 +0100

    vytvoreni rotace

--- Assets/Scenes/Dev2/Scripts/rotace.cs
+++ Assets/Scenes/Dev2/Scripts/rotace.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class rotace : MonoBehaviour
 {
+    [SerializeField]
+    int zivoty;
+    [SerializeField]
+    int sila;
+    [SerializeField]
+    Transform headTransform;
+
+    [SerializeField]
+    Transform legTransform;
+
+    [SerializeField]
+    Transform bodyTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,16 @@ public class rotace : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Toc(bodyTransform, false);
+        Toc(legTransform, true);
+        Toc(headTransform, true);
+    }
+    private void Toc(Transform tf,bool pravo)
+    {
+        if(pravo)
+        tf.Rotate(0f, zivoty * sila,0f );
+        if(!pravo)
+        tf.Rotate(0f, -zivoty * sila,0f);
+
     }
 }
