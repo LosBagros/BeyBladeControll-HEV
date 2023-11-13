1626a89078c0c772ed11b432c299621767d38a35
lagemaxl <lagemaxlcz@gmail.com>
2023-11-13 09:06:39 +0100

    edit: fix dev2 secene

--- Assets/Scenes/Dev2/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Dev2/Scripts/PlayerMovement.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float Speed;
+    [SerializeField] private float TurnSpeed = 180f;
     [SerializeField] private string InputNameHorizontal;
     [SerializeField] private string InputNameVertical;
 
     private Rigidbody rb;
-    private float InputHorizontal;
-    private float InputVertical;
+    private float inputHorizontal;
+    private float inputVertical;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
-    public void Update()
+
+    private void Update()
     {
-        //InputHorizontal = 
+        inputHorizontal = Input.GetAxis(InputNameHorizontal);
+        inputVertical = Input.GetAxis(InputNameVertical);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // Pohyb vpøed a vzad
+        float move = inputVertical * Speed * Time.deltaTime;
+
+        // Otáèení
+        float turn = inputHorizontal * TurnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Aplikace pohybu a otáèení
+        rb.MovePosition(rb.position + transform.forward * move);
+        rb.MoveRotation(rb.rotation * turnRotation);
     }
 }
